e948e3ca3421e61f5e951301086522d002f977e2
Julio Cesar <juliozulu2008@gmail.com>
2023-12-13 23:13:11 -0300

    Introdução as Rotas Controllers e Views, Avancando com Rotas

---
+++ resources/views/site/contato.blade.php
@@ -0,0 +1,6 @@
+<h3>Contato</h3>
+<ul>
+    <li><a href="{{ route('site.index') }}">Principal</a></li>
+    <li><a href="{{ route('site.sobrenos') }}">Sobre-nos</a></li>
+    <li><a href="{{ route('site.contato') }}">Contato</a></li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [PrincipalController::class, 'index'])->name('site.index');
+Route::get('/sobre-nos', [SobreNosController::class, 'index'])->name('site.sobrenos');
+Route::get('/contato', [ContatoController::class, 'index'])->name('site.contato');
+Route::get('/login', function () {
+    return 'Login';
+})->name('site.login');
+
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    })->name('app.clientes');
+    Route::get('/fornecedores', function () {
+        return 'Fornecedores';
+    })->name('app.fornecedores');
+    Route::get('/produtos', function () {
+        return 'Produtos';
+    })->name('app.produtos');
+});
+
+
+//parametros nas Rotas
+// Route::get('/contato/{nome}/{categoria_id}', function (string $nome, int $categoria) {
+//     echo "Estamos aqui, $nome, $categoria.";
+// })->where('nome', '[A-Za-z]+')->where('categoria_id', '[0-9]+'); //regex
+
+//Redirecionamento de Rotas
+Route::get('/rota1', function () {
+    return redirect()->route('site.index');
+})->name('site.rota1');
+Route::get('/rota2', function () {
+    echo 'Rota 2';
+})->name('site.rota2');
+Route::redirect('/rota2', '/rota1');
+//Fallback  Caso o usuario va para uma rota inexistente
+Route::fallback(function () {
+    echo 'A Rota Acessada não existe. <a href="' . route('site.index') . '">Clique aqui</a> para ir para a pagina inicial';
 });
